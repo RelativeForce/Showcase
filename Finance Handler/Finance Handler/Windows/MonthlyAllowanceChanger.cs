1130848a874ae44e3f650a5b99f3b4259377cccf
RelativeForce <joshiceddy@hotmail.com>
2017-10-11 16:51:39 +0100

    Added Comments an Documentation to MonthlyAllowance Changer Window

--- Finance Handler/Finance Handler/Windows/MonthlyAllowanceChanger.cs
+++ Finance Handler/Finance Handler/Windows/MonthlyAllowanceChanger.cs
@@ -27,6 +27,12 @@ namespace Finance_Handler.Windows
         /// </summary>
         private ToolTipHandler toolTipHandler;
 
+        /// <summary>
+        /// Constructs an new <see cref="MonthlyAllowanceChanger"/> using a DateTime 
+        /// that specifies the inital month the window will display.
+        /// </summary>
+        /// <param name="dataDestination">A reference to the object the data from this will be sent to.</param>
+        /// <param name="date">The inital month the window will display.</param>
         public MonthlyAllowanceChanger(Buffered dataDestination, DateTime date)
         {
             // Check argumants
@@ -59,33 +65,57 @@ namespace Finance_Handler.Windows
             // Do nothing as this is a child of the Main Window
         }
 
+        /// <summary>
+        /// Handles the user clicking the submit button on the <seealso cref="MonthlyAllowanceChanger"/>.
+        /// </summary>
+        /// <param name="sender">Unused.</param>
+        /// <param name="e">Unused.</param>
         private void submit_Click(object sender, EventArgs e)
         {
 
             try
             {
 
+                // If the user has inputted a monthly allowance that is different to the current one.
                 if (!oldAllowanceBox.Text.Equals(newAllowanceBox.Text))
                 {
+                    // If the user has not left the new monthly allowance text box empty.
                     if (!(newAllowanceBox.Text.Equals(string.Empty)))
                     {
+                        // Parse the new monthly allowance as a double value.
                         double newBudget = Double.Parse(newAllowanceBox.Text);
 
-                        Row newRow = new Row();
+                        // If the monthly allowance is greater than zero.
+                        if (newBudget > 0)
+                        {
 
-                        newRow.addColoumn(Budget.MONTH_COLOUMN, (month.Value.Month < 10 ? "0" + month.Value.Month : "" + month.Value.Month) + "" + month.Value.Year);
+                            // Create a row that will be added to the budget table.
+                            Row newRow = new Row();
 
-                        newRow.addColoumn(Budget.AMOUNT_COLOUMN, "" + newBudget);
+                            // Add the monthly allowance an the month it is assigned to.
+                            newRow.addColoumn(Budget.MONTH_COLOUMN, (month.Value.Month < 10 ? "0" + month.Value.Month : "" + month.Value.Month) + "" + month.Value.Year);
+                            newRow.addColoumn(Budget.AMOUNT_COLOUMN, "" + newBudget);
 
-                        Packet data = new Packet(this, dataDestination, newRow);
+                            // Add the row to the packet as data.
+                            Packet data = new Packet(this, dataDestination, newRow);
 
-                        dataDestination.recieve(data);
-
-                        this.Dispose();
+                            // Send the data to the main form and close this window.
+                            dataDestination.recieve(data);
+                            this.Dispose();
+                        }
+                        else
+                        {
+                            // Feedback to user.
+                            toolTipHandler.draw(
+                                "Error",
+                                "New Monthly allowance must be greater than zero.",
+                                newAllowanceBox
+                                );
+                        }
                     }
                     else
                     {
-
+                        // Feedback to user.
                         toolTipHandler.draw(
                         "Error",
                         "No new Allowance specified. Please enter a new allowance or 'Cancel'.",
@@ -96,7 +126,7 @@ namespace Finance_Handler.Windows
                 }
                 else
                 {
-
+                    // Feedback to user.
                     toolTipHandler.draw(
                         "Error",
                         "Allowance is unchanged. Please enter a new allowance or 'Cancel'.",
@@ -105,13 +135,10 @@ namespace Finance_Handler.Windows
 
 
                 }
-
-
-
             }
             catch (Exception ex)
             {
-
+                // Feedback to user.
                 toolTipHandler.draw(
                         "Error",
                         ex.Message,
@@ -119,23 +146,31 @@ namespace Finance_Handler.Windows
                         );
 
             }
-
-
-
         }
 
+        /// <summary>
+        /// Handles the user clicking the cancel button which results in closing the form.
+        /// </summary>
+        /// <param name="sender">Unused.</param>
+        /// <param name="e">Unused.</param>
         private void cancel_Click(object sender, EventArgs e)
         {
             this.Dispose();
         }
 
+        /// <summary>
+        /// Updates the current budget displayed to reflect the date selected in the date time picker.
+        /// </summary>
+        /// <param name="date">Date that denotes the budget.</param>
         private void updateBudget(DateTime date)
         {
-
+            // Converts the date into a month code. MM format
             string monthCode = (date.Month < 10 ? "0" + date.Month : "" + date.Month) + "" + date.Year;
 
+            // Retrieves the value of that month budget from the database.
             double currentValue = Budget.getInstance().getBudget(monthCode);
 
+            // If there actually is no value for that month display a dash. Otherwise display the value.
             if (currentValue.Equals(Double.NaN))
             {
                 oldAllowanceBox.Text = "-";
@@ -147,26 +182,51 @@ namespace Finance_Handler.Windows
 
         }
 
+        /// <summary>
+        /// Recommend a value for the user based on there previous transactions.
+        /// </summary>
+        /// <param name="sender">Unused.</param>
+        /// <param name="e">Unused.</param>
         private void recommendButton_Click(object sender, EventArgs e)
         {
             //TODO: Recommend a allowance based on a saving goal or rate of spendature.
         }
 
+        /// <summary>
+        /// When the value in the month date time picker is changed update the budget to reflect the newly specified month.
+        /// </summary>
+        /// <param name="sender">Unused.</param>
+        /// <param name="e">Unused.</param>
         private void month_ValueChanged(object sender, EventArgs e)
         {
             updateBudget(month.Value);
         }
 
+        /// <summary>
+        /// Closes the form when the form is closed.
+        /// </summary>
+        /// <param name="sender">Unused.</param>
+        /// <param name="e">Unused.</param>
         private void MonthlyAllowanceChanger_FormClosed(object sender, FormClosedEventArgs e)
         {
             this.Dispose();
         }
 
+        /// <summary>
+        /// Displays the tool tip over the amount text box.
+        /// </summary>
+        /// <param name="sender">Unused.</param>
+        /// <param name="e">Unused.</param>
         private void showAmountToolTip(object sender, EventArgs e)
         {
             toolTipHandler.draw("Input Allowance", "Input a new monthly allowance for the selected month.", newAllowanceBox);
         }
 
+        /// <summary>
+        /// Displays the recommend tool tip over the recomend button.
+        /// </summary>
+        /// <param name="sender">Unused.</param>
+        /// <param name="e">Unused.</param>
         private void showRecommendToolTip(object sender, EventArgs e)
         {
             toolTipHandler.draw("Recommend", "Unavailable", recommendButton);
