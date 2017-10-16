bfde27102dc1879766ccca1c34f5d617ba8a55b2
RelativeForce <joshiceddy@hotmail.com>
2017-10-16 16:50:54 +0100

    Added README.txt

--- JavaExamples/src/snake/package-info.java
+++ JavaExamples/src/snake/package-info.java
@@ -1,5 +1,9 @@
 /**
+ * 
+ * Contains the retro game of Snake in which the player snake attempts to eat as
+ * much food as possible to score points.
+ * 
  * @author Joshua_Eddy
- *
+ * 
  */
 package snake;
